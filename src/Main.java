d91e5fb05d9223e7b247909fb8b237792076d99b
metehan elmas <metehanelmas@hotmail.com>
2022-01-19 18:27:00 +0300

    *Programa 3 kullanım hakkı eklenmiştir *Sonsuz çalışma döngüsü kaldırıldı

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,34 @@ public class Main {
         int bolenler=1;
         int total = 0;
         int i = 1;
+        int right = 3;
+while (right > 0) {
+    while (i <= nSayi) {
+        System.out.print("Bir sayı giriniz : ");
+        nSayi = input.nextInt();
+        i++;
 
-        while (i <= nSayi) {
-            System.out.print("Bir sayı giriniz : ");
-            nSayi = input.nextInt();
-            i++;
-
-            for (int j = 1; j < nSayi; j++) {
-                if (nSayi % j == 0) {
-                    bolenler = j;
-                    total += bolenler;
-                }
+        for (int j = 1; j < nSayi; j++) {
+            if (nSayi % j == 0) {
+                bolenler = j;
+                total += bolenler;
             }
-            if (bolenler == 0) {
-                System.out.println("Pozitif değer giriniz !");
-
-            } else if (total == nSayi) {
-                System.out.println(nSayi + " Mükemmel Sayıdır .");
-
-            } else {
-                System.out.println(nSayi + " Mükemmel Sayı Değildir .");
-            }
-            total=reset;
         }
+        if (bolenler == 0) {
+            System.out.println("Pozitif değer giriniz !");
+
+        } else if (total == nSayi) {
+            System.out.println(nSayi + " Mükemmel Sayıdır .");
+
+        } else {
+            System.out.println(nSayi + " Mükemmel Sayı Değildir .");
+        }right--;
+        total = reset;
+        if (right == 0 ) {
+            System.out.println("Program bitmiştir :)");
+            break;
+        }
+    }
+}
     }
 }
